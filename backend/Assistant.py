3b1138d9670884569a13e55153d2196b2033098c
Benoit Mathern <benoit.mathern@taic.org.nz>
2026-02-12 10:30:56 +1300

    Added docstrings

--- backend/Assistant.py
+++ backend/Assistant.py
@@ -282,6 +282,9 @@ class AssistantPrompts:
 
     @staticmethod
     def conversation_title():
+        """
+        Returns AI prompt to generate a short title that summarises the topic of the conversation
+        """
         return """
 You are part of a chatbot assistant at the Transport Accident Investigation Commission. You help users add titles to their conversation. You will receive the conversation and you are to respond with a 5 word summary of the conversation.
 Provide a title that will best help the user recall what the conversation was.
@@ -290,6 +293,9 @@ Just respond with the title and nothing else.
 
     @staticmethod
     def general_info(columns, rows, last_updated):
+        """
+        Returns general context of the user's query
+        """
         return f"""
 Below is general information to help you contextualise the user's query.
 
@@ -325,6 +331,9 @@ issues.
 
     @staticmethod
     def orient_plan_system(general_info):
+        """
+        Returns AI prompt to orient and plan the initial orient and plan analysis
+        """
         return f"""
 You are a expert working at the New Zealand transport accident investigation commission. Your job is to assistant employees of TAIC with their queries. The day is {datetime.now(timezone.utc)}. You should respond as if you are a senior accident investigator/research who is speaking to your colleagues.
 
@@ -336,6 +345,9 @@ You will be provided the conversation history including any function calls and o
 
     @staticmethod
     def act_system(general_info):
+        """
+        Returns AI prompt to generate the response to the prompt
+        """
         return f"""
 You are a expert working at the New Zealand transport accident investigation commission. Your job is to assistant employees of TAIC with their queries. The day is {datetime.now(timezone.utc)}. You should respond as if you are a senior accident investigator/research who is speaking to your colleagues. Keep your responses short and to the point.
 
@@ -349,6 +361,10 @@ If you choose to respond to the user, ensure you provide a concise and accurate 
 
 
 class Assistant:
+    """
+    Assistant class handles the chat conversation and AI calls.
+    """
+
     def __init__(
         self,
         searcher,
